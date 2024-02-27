b318067b9e33dba027821200af8365e08e942131
gin7018 <79063763+gin7018@users.noreply.github.com>
2024-02-27 09:50:25 -0500

    add mongodb entities for this messenger app

---
+++ src/model.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+export const User = mongoose.model('user', new mongoose.Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, unique: true, required: true}
+}));
+
+export const Message = mongoose.model('message', new mongoose.Schema({
+    sender: {type: String, required: true},
+    receiver: {type: String, required: true},
+    text: {type: String, required: true},
+    timestamp: {type: Date, require: true, default: Date.now()}
+}))
